1bfa7f56f0b1e3ef1784554fb871d0efae6a91d1
Tanawan-Supanurat <tanawan.supanurat@jp.fujitec.com>
2022-10-07 11:45:19 +0900

    change main router to "PartsMaster"

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router:{
+    base: '/PartsMaster',
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - PartsMaster',
@@ -22,7 +26,10 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    ],
+    base:{
+      href: 'router.base',
+    }
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
