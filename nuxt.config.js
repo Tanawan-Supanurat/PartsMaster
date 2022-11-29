82f0a17870d7da8ec66b563f83ba8f97b86b78e0
Tanawan-Supanurat <tanawan.supanurat@jp.fujitec.com>
2022-11-29 15:24:10 +0900

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,9 +71,10 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-
-  ],
+  plugins: [{
+    src: '@/plugins/vue-js-xlsx.js',
+    ssr: false
+  }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
